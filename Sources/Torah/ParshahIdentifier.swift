a03a14bd95e43f02a47f10121bba1f6244fd195a
Natanel Niazoff <nniazoff@icloud.com>
2019-08-21 18:03:12 -0400

    Add rest of parshah identifiers

--- Sources/Torah/ParshahIdentifier.swift
+++ Sources/Torah/ParshahIdentifier.swift
@@ -1,4 +1,65 @@
 public enum ParshahIdentifier {
   // MARK: - Genesis
-  case bereishit
+  case bereshit
+  case noach
+  case lechLecha
+  case vayera
+  case chayeSarah
+  case toldot
+  case vayetze
+  case vayishlach
+  case vayeshev
+  case miketz
+  case vayigash
+  case vayechi
+  
+  // MARK: - Exodus
+  case shemot
+  case vaera
+  case bo
+  case beshalach
+  case yitro
+  case mishpatim
+  case terumah
+  case tetzaveh
+  case kiTisa
+  case vayakhel
+  case pekudei
+  
+  // MARK: - Leviticus
+  case vayikra
+  case tzav
+  case shemini
+  case tazria
+  case metzora
+  case achareiMot
+  case kedoshim
+  case emor
+  case behar
+  case bechukotai
+  
+  // MARK: - Numbers
+  case bamidbar
+  case naso
+  case behaalotecha
+  case shelachLecha
+  case korach
+  case chukat
+  case balak
+  case pinchas
+  case matot
+  case masei
+  
+  // MARK: - Deuteronomy
+  case devarim
+  case vaetchanan
+  case ekev
+  case reeh
+  case shoftim
+  case kiTetze
+  case kiTavo
+  case nitzavim
+  case vayelech
+  case haazinu
+  case vezotHaberachah
 }
