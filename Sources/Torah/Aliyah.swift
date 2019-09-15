5d5f808fde9da844526cc564a8f9434fe99534b3
Natanel Niazoff <nniazoff@icloud.com>
2019-09-15 16:24:39 -0400

    Add verseIndices

--- Sources/Torah/Aliyah.swift
+++ Sources/Torah/Aliyah.swift
@@ -4,3 +4,7 @@ public struct Aliyah {
   public let book: Book
   public let verseRange: ClosedRange<VerseIndex>
 }
+
+public extension Aliyah {
+  var verseIndices: [VerseIndex] { Array(book.verseIndices[verseRange]) }
+}
